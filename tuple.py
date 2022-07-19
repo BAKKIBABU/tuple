2dd240c6afbb9d4dbe02a46fae6847402af310a4
BAKKIBABU <108388125+BAKKIBABU@users.noreply.github.com>
2022-07-19 16:12:06 +0530

    Add files via upload

---
+++ tuple.py
@@ -0,0 +1,28 @@
+#   tuple is aimmutable immutable ,means modifications cannot possible or made  tuple immutable data type
+# x=(1)
+# print(x,type(x))   #int
+# x=(1,)
+# print(x,type(x))    #'tuple'
+# x=1,2
+# print(x,type(x))      #tuple
+# pr=('apple','reliance','asus','dell','samsung')
+# print(pr)
+# print(pr,type(pr))     #tuple  
+# pr='apple','reliance','asus','dell','samsung'
+# print(pr)
+# print(pr,type(pr))   #tuple
+# pr[1]='motorolla' #typeerror: tuple object does not support item assignment
+# a='hello'
+# a=[0]='c'   SyntaxError:cannot assaign to literal 
+# n='hello','world'  
+# print(n,type(n))    # ('hello','world')  class tuple
+# print(n[2])     #indexerror: tuple index out of range
+# v=(1,4.2,'hello',[2,3],('H','D'),None,True)
+# Print(v,type(v))
+# v=(1,4.2,'hello',[2,3],('H','D'))
+# v[0]=21
+# print(v.count(4.2))    #1
+# print(v.index('hello'))  #2
+# print(v.index(32.7))  # valueerror:tuple.index(x):xnot in tuple
+
+
